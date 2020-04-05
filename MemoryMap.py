360a25dccc59ee4d4ed6d985c74c3ef9685c5d30
Ryan-Sciarabba <rhsciarabba@gmail.com>
2020-04-05 15:55:02 -0400

    Refs #4, Some error support added and more dictionaries

--- MemoryMap.py
+++ MemoryMap.py
@@ -1,4 +1,11 @@
-ChangedFlag = 0
-Speed = 1
-Angle = 2
-Score = 3
+ports = {"CngFlag" : 0,
+         "Speed"   : 1,
+         "Angle"   : 2,
+         "Score"   : 3,
+         "ax"      : 4,
+         "bx"      : 5,
+         "cx"      : 6,
+         "dx"      : 7,
+         "ex"      : 8,
+         "fx"      : 9
+}
--- codeParserLambda.py
+++ codeParserLambda.py
@@ -1,32 +1,43 @@
-from Bee_Memory import VM
+import Bee_Memory
 import pickle
+import sys
 import MemoryMap
 
 class CodeParser():
 
+    #Jump to line
     def jmp(self, terms):
-
         for lineNum, line in enumerate(self.userCode):
             line = self.userCode[lineNum].split()
             if line[0] == terms[1]:
                 self.pushFunction()
                 self.lineNum = lineNum
                 return
-            
+    #Bee flies    
     def fly(self):
         return
         #REMOVE COLLISION FROM BEE FOR SET TIME PERIOD
         #INCREASE SIZE OF BEE SPRITE AND DEPTH OF SHADOW THEN GO BACK TO NORMAL
 
-    def portToNum(self, terms):
-        for i, _ in enumerate(terms):
-            if terms[i] == "SPEED":
-                terms[i] = self.bee.opo(MemoryMap.Speed)
-            if terms[i] == "ANGLE":
-                terms[i] = self.bee.opo(MemoryMap.Angle)
 
+    #Change port to memory location
+    def portToLoc(self, terms):
+        for i in range(1, len(terms)):
+            if terms[i] in MemoryMap.ports:
+                terms[i] = MemoryMap.ports[terms[i]]
+                
+    #Change port to an int
+    def portToNum(self, terms):
+        for i in range(1, len(terms)):
+            if terms[i] in MemoryMap.ports:
+                terms[i] = self.bee.opo(MemoryMap.ports[terms[i]])
+            
+
+    #Less than
     def lst(self, terms):
-        self.portToNum(terms)
+        if not terms[3] == "jmp":
+            self.invalidSyntaxError(self.userCode[self.lineNum], terms)
+            
         if int(terms[1]) < int(terms[2]):
             for lineNum, line in enumerate(self.userCode):
                 line in self.userCode[lineNum].split()
@@ -35,9 +46,12 @@ class CodeParser():
                     self.pushFunction()
                     self.lineNum = lineNum
                     return
-
+                
+    #Less than or equal to
     def lte(self, terms):
-        self.portToNum(terms)
+        if not terms[3] == "jmp":
+            self.invalidSyntaxError(self.userCode[self.lineNum], terms)
+            
         if int(terms[1]) <= int(terms[2]):
             for lineNum, line in enumerate(self.userCode):
                 line in self.userCode[lineNum].split()
@@ -46,9 +60,12 @@ class CodeParser():
                     self.pushFunction()
                     self.lineNum = lineNum
                     return
-                
+
+    #Greater than
     def grt(self, terms):
-        self.portToNum(terms)
+        if not terms[3] == "jmp":
+            self.invalidSyntaxError(self.userCode[self.lineNum], terms)
+            
         if int(terms[1]) > int(terms[2]):
             for lineNum, line in enumerate(self.userCode):
                 line in self.userCode[lineNum].split()
@@ -57,9 +74,12 @@ class CodeParser():
                     self.pushFunction()
                     self.lineNum = lineNum
                     return
-                
+
+    #Greater than or equal to         
     def gte(self, terms):
-        self.portToNum(terms)
+        if not terms[3] == "jmp":
+            self.invalidSyntaxError(self.userCode[self.lineNum], terms)
+            
         if int(terms[1]) >= int(terms[2]):
             for lineNum, line in enumerate(self.userCode):
                 line in self.userCode[lineNum].split()
@@ -68,9 +88,12 @@ class CodeParser():
                     self.pushFunction()
                     self.lineNum = lineNum
                     return
-                
+
+    #Equal to        
     def eqt(self, terms):
-        self.portToNum(terms)
+        if not terms[3] == "jmp":
+            self.invalidSyntaxError(self.userCode[self.lineNum], terms)
+            
         if int(terms[1]) == int(terms[2]):
             for lineNum, line in enumerate(self.userCode):
                 line in self.userCode[lineNum].split()
@@ -78,9 +101,12 @@ class CodeParser():
                 if line == terms[4]:
                     self.lineNum = lineNum
                     return
-                
+
+    #Not equal to        
     def nte(self, terms):
-        self.portToNum(terms)
+        if not terms[3] == "jmp":
+            self.invalidSyntaxError(self.userCode[self.lineNum], terms)
+            
         if int(terms[1]) != int(terms[2]):
             for lineNum, line in enumerate(self.userCode):
                 line in self.userCode[lineNum].split()
@@ -90,6 +116,7 @@ class CodeParser():
                     self.lineNum = lineNum
                     return
 
+    #Error
     def err(self, terms):
         temp = ""
         for i in range(1, int(len(terms))): 
@@ -97,42 +124,53 @@ class CodeParser():
             temp += " "
         print("ERROR: " + temp)
 
+    #Wait cycles
     def wait(self, cycles):
+        if not isinstance(cycles, int):
+            self.invalidInputError(self.userCode[self.lineNum])
+            
         self.bee.nop(int(cycles))
 
+    #Push function to stack for return
     def pushFunction(self):
         self.functionLine.append(self.lineNum)
 
+    #Return to function or end code
     def popFunction(self):
-        if(len(self.functionLine) > 0):
+        if len(self.functionLine) > 0:
             self.lineNum = self.functionLine[(len(self.functionLine)) - 1]
             self.functionLine.pop()
             
-        elif(len(self.functionLine) == 0):
+        elif len(self.functionLine) == 0:
             self.lineNum = len(self.userCode) + 1
     
-    bee = VM()
-                                      
+    bee = Bee_Memory.VM()
+
+    #Initialization                             
     def __init__(self, path):
 
         #Set line number to zero and initialize stack
         self.lineNum = 0
         self.functionLine = []
-        
+
+        #Check if path is a text file
+        if not ".txt" in path:
+            self.fileTypeError()
+            
         #Open bee code file
         beeCode = open(path, "r")
         self.userCode = beeCode.readlines()
         beeCode.close()
 
-        #Remove whitespace
+        #Remove whitespace and comments and, set ports to memory locations 
         line = 0
         while line < len(self.userCode):
             terms = self.userCode[line].split()
-            if len(terms) == 0:
+            if len(terms) == 0 or "#" in terms[0]:
                 self.userCode.pop(line)
             else:
                 line += 1
-        
+      
         #Save bee code as binary
         binPath = path.replace(".txt", ".bin")
         binFile = open(binPath, "wb")
@@ -141,7 +179,8 @@ class CodeParser():
 
         #Create bee VM
         self.bee.print_ram()
-        
+
+    #Parser  
     def parse(self):
 
         #Function dictionary
@@ -154,7 +193,7 @@ class CodeParser():
                 "inc" : self.bee.inc,  #Working
                 "dec" : self.bee.dec,  #Working
                 "neg" : self.bee.neg,  #Working
-                "or" : self.bee.orr,   #Working
+                "or"  : self.bee.orr,  #Working
                 "and" : self.bee.andd, #Working
                 "xor" : self.bee.xorr, #Working
                 "not" : self.bee.nott  #Working
@@ -168,14 +207,12 @@ class CodeParser():
                         "gte" : self.gte, #Working
                         "eqt" : self.eqt, #Working
                         "nte" : self.nte, #Working
-                        "nop" : self.wait #Working
         }
         
         while self.lineNum < len(self.userCode):
 
-            #Split line up into parts
-            terms = self.userCode[self.lineNum].split()
-            ##print(self.lineNum, terms)           
+            #Split line up into parts and change register name to register location
+            terms = self.userCode[self.lineNum].split()       
                 
             #Find function
             if terms[0] == "end":
@@ -188,29 +225,32 @@ class CodeParser():
                 self.wait(terms[1])
                 
             elif len(terms) == 3:
-                
-                #Set memory location to what variable needs to be changed
-                if terms[1] == "SPEED":
-                    self.memloc = MemoryMap.Speed
-                elif terms[1] == "ANGLE":
-                    self.memloc = MemoryMap.Angle
-                    
-                math[terms[0]](self.memloc, int(terms[2]))
+                self.portToLoc(terms)
+                math[terms[0]](int(terms[1]), int(terms[2]))
 
             elif len(terms) == 5 or len(terms) == 2:
                 
                 #Set memory location to what variable needs to be changed
-                if terms[1] == "SPEED":
-                    self.memloc = MemoryMap.Speed
-                elif terms[1] == "ANGLE":
-                    self.memloc = MemoryMap.Angle
-                    
+                self.portToNum(terms)
                 if math.get(terms[0]):    
-                    math[terms[0]](self.memloc)
+                    math[terms[0]](terms[1], terms[2])
                 else:
                     comparisons[terms[0]](terms)
 
             #Increment line number
             self.lineNum += 1
 
-        
+    #Wrong filetype when loading bee
+    def fileTypeError(self):
+        print("ERROR: Invalid filetype for bee. Please use a .txt file")
+        sys.exit()
+
+    #Wrong input type with function
+    def invalidInputError(self, line):
+        print("ERROR: Invalid input type on line", self.lineNum, ":", line)
+        sys.exit()
+
+    #Wrong syntax with function
+    def invalidSyntaxError(self, line, terms):
+        print("ERROR: Invalid syntax on line", self.lineNum, ":", line, "...'", terms[3], "' should be ' jmp '")
+        sys.exit()
