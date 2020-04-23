e23064b54c393389893e9c01e7f815e326c3b7db
JEC-SCHOOL <60239609+JEC-SCHOOL@users.noreply.github.com>
2020-04-23 14:27:28 -0400

    Add files via upload

---
+++ beeracer/settings.py
@@ -0,0 +1,34 @@
+import pygame as pg 
+
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+
+# game settings
+WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
+FPS = 60
+TITLE = "Bee Game"
+BGCOLOR = DARKGREY
+
+TILESIZE = 64
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+# Player settings
+PLAYER_SPEED = 300
+PLAYER_ROT_SPEED = 250
+PLAYER_IMG = 'Bee.png'
+PLAYER_HIT_RECT = pg.Rect(0,0,35,35)
+PLAYER_START_POLLEN = 0
+PLAYER_MAX_POLLEN = 100
+
+# Pollen
+POLLEN_IMAGES = {'pollen' : 'Honey.png'}
+POLLEN_AMOUNT = 20
